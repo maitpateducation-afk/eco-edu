26c026f9a86b636e79b45e06c292f88c31c73b14
maitpateducation-afk <maitpateducation@gmail.com>
2026-06-22 18:43:03 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,9 @@
-// Service Worker v5 - Force network always
-const CACHE = 'eco-edu-v5';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+// Ce service worker se désactive lui-même
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  // Always go to network, never use cache
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
+    .then(() => self.registration.unregister())
   );
 });
