dc79f9fb66a771ea143aadd6f263d2ee1d2e7c79
maitpateducation-afk <maitpateducation@gmail.com>
2026-06-24 11:27:36 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(n => caches.delete(n)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
