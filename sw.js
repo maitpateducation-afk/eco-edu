15bafbce54ddf1bfb94042b4b65f517ca93cc680
maitpateducation-afk <maitpateducation@gmail.com>
2026-06-24 11:04:56 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,1 @@
-// Ce service worker se désactive lui-même
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
-    .then(() => self.registration.unregister())
-  );
-});
+
